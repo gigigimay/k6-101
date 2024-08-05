b1ac3edfa3704c004df2c571a91a8fd1dce2b863
gigigimay <51327193+gigigimay@users.noreply.github.com>
2024-08-05 20:03:21 +0700

    remove credentials from qa + uat env

--- env.js
+++ env.js
@@ -7,27 +7,7 @@ const env = {
     operationKCClientId: "mac-application-manager",
     operationKCUsername: "gigi",
     operationKCPassword: "Gigi",
-  },
-  qa: {
-    backgroundCheckBaseUrl:
-      "https://portal-qa.mac-non-prod.appmanteam.com/api/v1/background-check",
-    backgroundCheckApiKey: "NDE2ZjNhYzMtYzlhYy00YWYwLWI1MjQtZjE3ZDkwYzMzOTU0",
-    kcUrl: "https://portal-qa.mac-non-prod.appmanteam.com/auth",
-    operationKCRealm: "mac-application-admin",
-    operationKCClientId: "mac-application-manager",
-    operationKCUsername: "gb_supervisor_1@appman.co.th",
-    operationKCPassword: "!QAZ2wsx",
-  },
-  uat: {
-    backgroundCheckBaseUrl:
-      "https://portal-uat.mac-non-prod.appmanteam.com/api/v1/background-check",
-    backgroundCheckApiKey: "MjZlNWJlMzEtNGMwNy00ZjUxLTg1MTgtOWVlZTIzYzUwNmUx",
-    kcUrl: "https://portal-uat.mac-non-prod.appmanteam.com/auth",
-    operationKCRealm: "mac-application-admin",
-    operationKCClientId: "mac-application-manager",
-    operationKCUsername: "jidapa.o",
-    operationKCPassword: "!QAZ2wsx",
-  },
+  }
 };
 
-export default env.uat;
+export default env.local;
